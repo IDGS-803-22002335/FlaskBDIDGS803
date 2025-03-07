1a2bd32933a230bc6a0261608b19691b77e12a02
IDGS-803-22002335 <tovardiazjuanaesmeralda@gmail.com>
2025-03-06 22:18:29 -0600

    Agregar

--- app.py
+++ app.py
@@ -22,7 +22,39 @@ def page_not_found(e):
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    create_form = forms.UserForm2(request.form)
+    alumno = Alumnos.query.all()  # generar la consulta para traer a los alumnos
+
+    return render_template("index.html", form=create_form, alumnos=alumno)
+
+# viene en el examen
+
+
+@app.route('/detalles', methods=['GET', 'POST'])
+def detalles():
+    create_form = forms.UserForm2(request.form)
+    if request.method == 'GET':
+        id = request.args.get('id')
+        alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        id = request.args.get('id')
+        nombre = alum1.nombre
+        apaterno = alum1.apaterno
+        email = alum1.email
+    return render_template('detalles.html', id=id, nombre=nombre, apaterno=apaterno, email=email)
+
+
+@app.route('/Alumnos1', methods=['GET', 'POST'])
+def Alumnos1():
+    create_form = forms.UserForm2(request.form)
+    if request.method == 'POST':
+        alum = Alumnos(nombre=create_form.nombre.data,
+                       apaterno=create_form.apaterno.data,
+                       email=create_form.email.data)
+
+        db.session.add(alum)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('Alumnos1.html', form=create_form)
 
 
 if __name__ == '__main__':
